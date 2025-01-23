12dedb1db0826085ae750c97d44687652343c6ce
bionapta <aaptazaimm@gmail.com>
2025-01-23 08:03:41 +0700

    fix promise for CategoryPageProps and ProductPageProps

--- app/(routes)/page.tsx
+++ app/(routes)/page.tsx
@@ -1,21 +1,21 @@
-import getBanners from '@/actions/get-banner';
-import getProducts from '@/actions/get-products';
-import Banner from '@/components/banner';
-import ProductList from '@/components/product-list';
-import Container from '@/components/ui/container';
+import getBanner from "@/actions/get-banner";
+import getProducts from "@/actions/get-products";
+import Banner from "@/components/banner";
+import ProductList from "@/components/product-list";
+import Container from "@/components/ui/container";
 
 export const revalidate = 0;
 
 const HomePage = async () => {
   const products = await getProducts({ isFeatured: true });
-  const banner = await getBanners('646c8c42-0a3f-41ce-b3b2-b57684ed85a2');
+  const banner = await getBanner("269242a0-744a-4ec7-a8b2-00210cbc7f79");
 
   return (
     <Container>
       <div className="space-y-10 pb-10">
         <Banner data={banner} />
         <div className="flex flex-col gap-y-8 px-4 sm:px-6 lg:px-8">
-          <ProductList title="Top Items" items={products} />
+          <ProductList title="Produk Unggulan" items={products} />
         </div>
       </div>
     </Container>
--- types.ts
+++ types.ts
@@ -23,3 +23,12 @@ export interface Image {
   id: string;
   url: string;
 }
+
+type SegmentParams<T extends Record<string, any> = any> = {
+  [K in keyof T]: T[K] extends string ? string | string[] | undefined : never;
+};
+
+export interface PageProps {
+  params?: SegmentParams;
+  searchParams?: any;
+}
